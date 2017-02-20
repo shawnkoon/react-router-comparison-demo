3874dd59bb575e3d2a910809eb0f660bd9404461
shawnkoon <shawn9405@gmail.com>
2017-02-20 12:54:32 -0800

    Added react boilerplate with minor change of app.js

--- public/appV3.jsx
+++
@@ -1,4 +0,0 @@
-ReactDOM.render(
-	<h1>This is version 3</h1>,
-	document.getElementById('app')
-);
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var express = require('express');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 
-// Create our app.
-var app = express();
-
-app.use(express.static('public'));
-
-app.listen(8000, function() {
-	console.log('Express server is up on port 8000');
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(5000, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Listening at localhost:5000');
 });
